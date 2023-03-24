cf53d6129d866e1f6ed565fbd22a1196224f955b
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-03-24 16:03:40 -0700

    Restructured the initial code provided by apple when using CoreData to conform to our preferred structure.

---
+++ Devote/Utility/Contant.swift
@@ -0,0 +1,23 @@
+// Contant.swift
+// Devote
+// 
+// Created by Jonathan Schaffer
+// Using Swift 5.0
+//
+// https://jonathanschaffer.com
+// Copyright © 2023 Jonathan Schaffer. All rights reserved.
+
+import SwiftUI
+
+// MARK: - FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
