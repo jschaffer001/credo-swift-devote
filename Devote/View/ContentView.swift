3f4160b33ef6b9ff154b2a901b5a7718a7a99994
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-04-05 14:04:07 -0700

    BUG FIX: Figured out an alternative way to make the background of the table view transparent that actually works as opposed to the UITableView method.

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -64,55 +64,70 @@ struct ContentView: View {
     // MARK: - BODY
     var body: some View {
         NavigationView {
-            VStack {
-                VStack(spacing: 16) {
-                    TextField("New Task", text: $task)
-                        .padding()
-                        .background(Color(UIColor.systemGray6))
-                        .cornerRadius(10)
-                    
-                    Button(action: {
-                        addItem()
-                    }, label: {
-                        Spacer()
-                        Text("SAVE")
-                        Spacer()
-                    }) //: BUTTON
-                    .disabled(isButtonDisabled) // elegant modifier used with computed property to disable the button
-                    .padding()
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .background(isButtonDisabled ? Color.gray : Color.pink)
-                    .cornerRadius(10)
-                } //: VSTACK
-                .padding()
-                
-                List {
-                    ForEach(items) { item in
-                        VStack(alignment: .leading
-                        ) {
-                            Text(item.task ?? "")
-                                .font(.headline)
-                                .fontWeight(.bold)
-                            
-                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                                .font(.footnote)
-                                .foregroundColor(.gray)
-                        } //: VSTACK - LIST ITEM
+            ZStack {
+                VStack {
+                    VStack(spacing: 16) {
+                        TextField("New Task", text: $task)
+                            .padding()
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(10)
                         
-                    } //: LOOP
-                    .onDelete(perform: deleteItems)
-                } //: LIST
-            } //: VSTACK
+                        Button(action: {
+                            addItem()
+                        }, label: {
+                            Spacer()
+                            Text("SAVE")
+                            Spacer()
+                        }) //: BUTTON
+                        .disabled(isButtonDisabled) // elegant modifier used with computed property to disable the button
+                        .padding()
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .background(isButtonDisabled ? Color.gray : Color.pink)
+                        .cornerRadius(10)
+                    } //: VSTACK
+                    .padding()
+                    
+                    List {
+                        ForEach(items) { item in
+                            VStack(alignment: .leading
+                            ) {
+                                Text(item.task ?? "")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                            } //: VSTACK - LIST ITEM
+                            .listRowBackground(Color.clear) // Used in conjuction with changing the listStyle from InsetGroupedList Style to PlainListStlye to make the background of each row of the list transparent.
+                        } //: LOOP
+                        .onDelete(perform: deleteItems)
+                    } //: LIST
+                    //.listStyle(InsetGroupedListStyle())
+                    .listStyle(PlainListStyle()) // Used in conjuction with the listRowBackground and background Color.clear modifiers to achieve a transparent background. This does change the list style from a InsetGroupedListStyle to a PlainListStyle which may have other impacts but we will have to see.
+                    .background(Color.clear) // Used in conjuction with changing the listStyle from InsetGroupedList Style to PlainListStlye to make the background of the entire list transparent as well as each row as seen in the listRowBackground modifier.
+                    
+                    
+                } //: VSTACK
+            } //: ZSTACK
+            // Commented out the following because it does not seem to work any longer when trying to make the background of the list transparent
+            // Instead the listStyle was changed to a PlainListStyle rather than a InsetGroupedListStyle with listRow and list backgrounds changed to Color.clear
+            // May impact the app in other ways but seems to achieve the desired effect with minimal impact.
+            //.onAppear() {
+            //    //UITableView.appearance().backgroundColor = UIColor.clear
+            //}
             .navigationBarTitle("Daily Tasks", displayMode: .large)
             .toolbar {
-                #if os(iOS)
+                //#if os(iOS)
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
-                #endif
+                //#endif
             } //: TOOLBAR
+            .background(backgroundGradient.ignoresSafeArea())
         } //: NAVIGATION
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 
     
