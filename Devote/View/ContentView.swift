dd13275036d0816abf395f668fd4ebc398f5cebd
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-04-24 11:49:07 -0700

    added a blur effect to the background image as well as the list items when the new item view is visible to enhance focus

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -122,10 +122,16 @@ struct ContentView: View {
                     .frame(maxWidth: 640)
                     .cornerRadius(16)
                 } //: VSTACK
+                .blur(radius: showNewTaskItem ? 8.0 : 0.0, opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeInOut(duration: 0.5))
+                
                 // MARK: - NEW TASK ITEM
                 
                 if showNewTaskItem {
-                    BlankView()
+                    BlankView(
+                        backgroundColor: isDarkMode ? Color.black : Color.gray,
+                        backgroundOpacity: isDarkMode ? 0.3 : 0.5)
                         .onTapGesture {
                             withAnimation() {
                                 showNewTaskItem = false
@@ -146,6 +152,7 @@ struct ContentView: View {
             .navigationBarHidden(true)
             .background(
                 BackgroundImageView()
+                    .blur(radius: showNewTaskItem ? 8.0 : 0.0, opaque: false)
             )
             .background(backgroundGradient.ignoresSafeArea())
         } //: NAVIGATION
