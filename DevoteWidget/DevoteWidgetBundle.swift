14aad46c4d1c8cd1f019351e73f25de39aab545b
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-04-25 15:26:02 -0700

    Added a new widget target to enable the building of a Devote themed widget.

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,18 @@
+// DevoteWidgetBundle.swift
+// Devote
+// 
+// Created by Jonathan Schaffer
+// Using Swift 5.0
+//
+// https://jonathanschaffer.com
+// Copyright © 2023 Jonathan Schaffer. All rights reserved.
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
